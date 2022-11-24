55f8be42d46468e482b7b4c929b0b5cac457eef7
Lukas Einfalt <lukx@lukx.de>
2022-11-24 21:55:01 +0100

    writing rating to file

--- src/backend/model/database/sql/RatingManager.ts
+++ src/backend/model/database/sql/RatingManager.ts
@@ -1,10 +1,26 @@
 import {PhotoEntity} from "./enitites/PhotoEntity";
 import {IRatingManager} from "../interfaces/IRatingManager";
 import {SQLConnection} from "./SQLConnection";
-import {SQL_COLLATE} from "./enitites/EntityUtils";
+import {promises as fsp} from 'fs';
 import * as path from "path";
+import {ProjectPath} from "../../../ProjectPath";
+
+interface RatingBufferEntry {
+  fullPath: string,
+  rating: (0 | 1 | 2 | 3 | 4 | 5),
+}
 
 export class RatingManager implements IRatingManager {
+  private ratingBuffer: RatingBufferEntry[] = [];
+  private timeout: any = null;
+
+  public async writeBuffer(): Promise<void> {
+    const theContent = this.ratingBuffer.map((itm) => itm.rating + '|' + itm.fullPath).join("\n") + "\n";
+    this.ratingBuffer = [];
+
+    await fsp.appendFile(path.join(ProjectPath.ImageFolder, 'ratings_buffer.csv'), theContent);
+  }
+
   public async updateRating(mediaPath: string, newRating: 0 | 1 | 2 | 3 | 4 | 5): Promise<PhotoEntity> {
     const connection = await SQLConnection.getConnection();
     const repository = connection.getRepository(PhotoEntity);
@@ -27,7 +43,31 @@ export class RatingManager implements IRatingManager {
       .where('photo.name = :mediaName AND directory.name = :dirName', query)
       .getOne();
 
+    if (!photo) {
+      throw new Error('Photo not found');
+    }
+
     photo.metadata.rating = newRating;
+
+    const absoluteFileName = path.join(
+      ProjectPath.ImageFolder,
+      path.normalize(mediaPath)
+    );
+
+
+    this.ratingBuffer.push({
+      fullPath: absoluteFileName,
+      rating: newRating,
+    });
+
+    if (this.timeout !== null) {
+      clearTimeout(this.timeout)
+      this.timeout = null;
+    }
+
+    this.timeout = setTimeout(() => {
+      this.writeBuffer();
+    }, 6000);
 
     await repository.save(photo);
     return photo;
